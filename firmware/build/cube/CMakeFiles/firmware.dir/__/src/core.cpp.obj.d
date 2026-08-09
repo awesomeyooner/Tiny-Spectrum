89c7c790d7ec8fec8cb49ce99849a9ffab5237a5
Aaron Yoon <awesomeyooner@gmail.com>
2026-08-09 01:40:54 -0700

    added base stm32cubemx generated code

---
+++ firmware/build/cube/CMakeFiles/firmware.dir/__/src/core.cpp.obj.d
@@ -0,0 +1,3 @@
+cube/CMakeFiles/firmware.dir/__/src/core.cpp.obj: \
+ /home/aaron/Documents/GitHub/Tiny-Spectrum/firmware/src/core.cpp \
+ /home/aaron/Documents/GitHub/Tiny-Spectrum/firmware/include/core.hpp
---
+++ firmware/src/core.cpp
@@ -0,0 +1,13 @@
+#include "core.hpp"
+
+
+void init()
+{
+
+} // end of "init()"
+
+
+void update()
+{
+    
+} // end of "update()"
